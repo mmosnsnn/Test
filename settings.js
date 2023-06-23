205ac3ee6e4c9e744f76cb68b50819b1e6064acd
mmosnsnn <89577578+mmosnsnn@users.noreply.github.com>
2023-06-23 16:18:33 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,71 @@
+const chalk = require("chalk")
+const fs = require("fs")
+
+//documents variants
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+//owmner v card
+global.owner = ['233596371001'] //ur owner number
+global.ownernomer = "233206450709" //ur owner number2
+global.ownername = "Cyran Kyle" //ur owner name
+global.ytname = "null" //ur yt chanel name
+global.socialm = "GitHub: jake-net-hunter" //ur github or insta name
+global.location = "Ghana" //ur location
+
+//new
+global.botname = "Meta Bot MD V2"
+global.ownernumber = '233596371001'
+global.ownername = 'Cyran Kyle'
+global.ownerNumber = ["233596371001@s.whatsapp.net"]
+global.ownerweb = "https://cyran-kyle-web.netlify.app"
+global.websitex = "https://ycyrankyle.com"
+global.wagc = "https://chat.whatsapp.com/GeCtfVpcng8BBrcRDgvY8j"
+global.themeemoji = '🥀'
+global.wm = "Meta Bot inc."
+global.botscript = 'https://github.com/jake-net-hunter/xb' //script link
+global.packname = "Sticker By Meta Bot MD"
+global.author = "Meta Bot MD V2\n\n\nA whatsapp bot developed by\n Cyran Kyle\n\n--______--\n\nContact No. : +233596371001"
+global.creator = "233596371001@s.whatsapp.net"
+global.prefa = ['','!','.','#','&']
+global.hituet = 0
+
+//media target
+global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
+global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+
+//menu image maker
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+//messages
+global.mess = {
+    success: 'Here you go!',
+    admin: 'This feature could be used by admins only!',
+    botAdmin: 'Bot Must Be Admin First!',
+    premime: 'Premium Special Features If You Want to Register Type Rent',
+    owner: 'This feature could be used by owner only',
+    group: 'Features Used Only For Groups!',
+    private: 'Features Used Only For Private Chat!',
+    bot: 'This feature could be used by bot only',
+    wait: 'In process...',
+    linkm: 'Where is the link?',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
